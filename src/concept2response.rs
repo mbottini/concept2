9ad9d2e435ecd0ba5db7bd6c321613782fc2ee0f
Michael Bottini <mbottini1@gmail.com>
2021-06-03 23:21:36 -0700

    Flesh out parsing section

--- src/concept2response.rs
+++ src/concept2response.rs
@@ -1,5 +1,6 @@
 use crate::consts;
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum Concept2Response {
     GetStatus,
     GetVersion,
@@ -25,21 +26,32 @@ impl ResponseFrame {
     }
 }
 
-fn parse_c2r<T>(iter: &mut T) -> Option<ResponseFrame> 
-    where T: Iterator<Item=u8> {
+fn unopt<T>(iter: &mut impl Iterator<Item = Option<T>>) -> Option<Vec<T>> {
+    let mut result: Vec<T> = vec![];
+    while let Some(v) = iter.next() {
+        match v {
+            Some(value) => result.push(value),
+            None => return None,
+        }
+    }
+    Some(result)
+}
+
+fn parse_c2r<'a, T>(iter: &mut T) -> Option<Concept2Response> 
+    where T: Iterator<Item=&'a u8> {
     let status = iter.next();
     let identifier = iter.next();
     let bytes = iter.next();
     match (status, identifier, bytes) {
         (Some(s), Some(i), Some(b)) => {
-            let data: Vec<u8> = iter.take(usize::from(b)).collect();
-            if data.len() == usize::from(b) {
-                Some(ResponseFrame {
-                    status: s,
-                    identifier: i,
-                    bytes: b,
+            let data: Vec<u8> = iter.take(usize::from(*b)).map(|&x| x).collect();
+            if data.len() == usize::from(*b) {
+                ResponseFrame {
+                    status: *s,
+                    identifier: *i,
+                    bytes: *b,
                     data: data,
-                })
+                }.parse()
             } else {
                 None
             }
@@ -48,30 +60,37 @@ fn parse_c2r<T>(iter: &mut T) -> Option<ResponseFrame>
     }
 }
 
-fn parse_helper<T>(iter: &mut T) -> Option<Vec<ResponseFrame>> 
-    where T: Iterator<Item=u8> {
+fn parse_helper<'a>(iter: &mut impl Iterator<Item=&'a u8>) -> Option<Vec<Concept2Response>> {
     let mut result = vec![];
     while let Some(current_head) = iter.next() {
-        if current_head == consts::CSAFE_STOP_FLAG {
+        if current_head == &consts::CSAFE_STOP_FLAG {
             return Some(result);
         }
         let curr = parse_c2r(iter);
         match curr {
-            Some(frame) => result.push(frame),
+            Some(c2r) => result.push(c2r),
             None => return None,
         }
     }
     None
 }
 
-/*
 fn parse_vec(v: &Vec<u8>) -> Option<Vec<Concept2Response>> {
     let mut vec_iter = v.iter();
     let report_num = vec_iter.next();
     let start_flag = vec_iter.next();
+    match start_flag {
+        Some(&consts::CSAFE_START_FLAG) => parse_helper(&mut vec_iter),
+        _ => None
+    }
 }
-*/
+
 /*
-pub fn parseVec(Vec<u8> &v) -> Option<Concept2Response> {
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn parse_serial_number() {
+        assert_eq!(vec![super::Concept2Response::GetSerialNumber(""])
+    }
 }
 */
