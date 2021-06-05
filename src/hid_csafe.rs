bc5a6f35cbcc25a305cef7b4b146b1db3d42a359
Michael Bottini <mbottini1@gmail.com>
2021-06-04 23:26:12 -0700

    Format hid_csafe

--- src/hid_csafe.rs
+++ src/hid_csafe.rs
@@ -1,18 +1,23 @@
 use hidapi::{HidDevice, HidResult};
 
-use crate::csafe;
 use crate::concept2response;
+use crate::csafe;
 
 const MSG_LENGTH: usize = 121;
 
-pub fn write_read_csafe_cmd(device: &HidDevice, report_num: u8, cmd: &csafe::CSAFEFrame) -> HidResult<Vec<u8>> {
+pub fn write_read_csafe_cmd(
+    device: &HidDevice,
+    report_num: u8,
+    cmd: &csafe::CSAFEFrame,
+) -> HidResult<Vec<u8>> {
     let msg: Vec<u8> = std::iter::once(report_num)
         .chain(cmd.to_vec().into_iter())
         .chain(std::iter::repeat(0))
         .take(MSG_LENGTH)
         .collect();
     let mut recv_result: Vec<u8> = vec![0; MSG_LENGTH];
-    device.write(msg.as_slice())
+    device
+        .write(msg.as_slice())
         .map(|_| device.read(recv_result.as_mut_slice()))
         .map(|_| recv_result)
 }
