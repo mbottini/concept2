88fde7f598c8a95a159531e8a93bd7e2eab4ceec
Michael Bottini <mbottini1@gmail.com>
2021-06-09 19:54:21 -0700

    Remove extraneous test from lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,3 @@ pub mod concept2response;
 pub mod consts;
 pub mod csafe;
 pub mod hid_csafe;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
